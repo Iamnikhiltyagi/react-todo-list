41cc4716b15229e7637592d1b172a20fb2beca76
Iamnikhiltyagi <nt14434@gmail.com>
2020-08-19 13:23:05 +0530

    first commit

---
+++ src/components/TodoInputs.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class todoinputs extends Component {
+    render() {
+        return (
+            <h1>
+                Hello from input
+            </h1>
+        )
+    }
+}
