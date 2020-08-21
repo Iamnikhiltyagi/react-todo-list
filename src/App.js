591ddd1fe4139870ab4bbc554cf62bef98b78191
Iamnikhiltyagi <nt14434@gmail.com>
2020-08-21 14:20:38 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInputs from "./components/TodoInputs";
 import TodoList from "./components/TodoList";
 
-function App() {
-  return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <TodoInputs />
-          <TodoList />
+class App extends Component {
+  state = {
+    items: [
+      { id: 1, title: "wake up" },
+      { id: 2, title: "make breakfast" },
+    ],
+    id: uuidv4(),
+    item: "",
+    editItem: false,
+  };
+
+  handleChange = (e) => {
+    console.log("handle change");
+  };
+  handleSubmit = (e) => {
+    console.log("handle Submit");
+  };
+  clearList = () => {
+    console.log("Clear List");
+  };
+  handleDelete = (id) => {
+    console.log(`handle delete  ${id}`);
+  };
+  handleEdit = (id) => {
+    console.log(`handle Edit  ${id}`);
+  };
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">todo input</h3>
+
+              <TodoInputs
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
