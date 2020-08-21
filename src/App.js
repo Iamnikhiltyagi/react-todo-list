a56ce2fff4a571fedc264c3cefc9e9f3c5daa4f7
Iamnikhiltyagi <nt14434@gmail.com>
2020-08-21 12:52:21 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInputs from "./components/TodoInputs";
